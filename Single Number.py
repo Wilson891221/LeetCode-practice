c37950c43975e35982548ac615e9fac601c668d9
Wilson <79236612+wilson891221@users.noreply.github.com>
2021-08-24 10:41:33 +0800

    Update Single Number.py

--- Single Number.py
+++ Single Number.py
@@ -2,6 +2,6 @@ class Solution:
     def singleNumber(self, num: List[int]) -> int:
         temp = 0 
         for i in range(len(num)):
-            temp ^= num[i]
+            temp ^= num[i] #^= is doing XOR
         return temp
                 
