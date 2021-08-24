6f9d8eba73d5cfc525b7b4a48ce18de44e1c383c
Wilson <79236612+wilson891221@users.noreply.github.com>
2021-08-24 10:22:20 +0800

    Add files via upload

---
+++ Sqrt(x).cpp
@@ -0,0 +1,24 @@
+class Solution {
+public
+    int mySqrt(int x) {
+        long long int temp = 0;
+        long long int i=0;
+        while(i!= UINT_MAX){
+
+            if(ii==x){
+                return i;
+            }
+            else{
+                if(tempx&& xii){
+                    return i-1;
+                }
+                else{
+                    temp = ii;
+                }
+
+            }
+            i++;
+        }
+        return 0;
+    }
+};
